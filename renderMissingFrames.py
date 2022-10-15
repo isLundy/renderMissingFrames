5b740b0c121ba014b00ac60611308f7958dbcecb
Lundy Hu <lundyhu@outlook.com>
2022-10-16 02:19:05 +0800

    Update renderMissingFrames.py

--- renderMissingFrames.py
+++ renderMissingFrames.py
@@ -1,7 +1,7 @@
 import os
 from PySide2.QtWidgets import (QWidget, QLineEdit, QPushButton, QApplication,
         QLayout, QGridLayout, QLabel, QHBoxLayout, QVBoxLayout, QDesktopWidget)
-from PySide2.QtGui import QCursor
+from PySide2.QtGui import QIntValidator
 from PySide2.QtCore import Qt
 
 class RenderMissingFrames(QWidget):
@@ -32,6 +32,9 @@ class RenderMissingFrames(QWidget):
             self.endText.setPlaceholderText('check end frame')
             self.endText.setText(str(int(nuke.root()['last_frame'].getValue())))
 
+            self.startText.setValidator(QIntValidator())
+            self.endText.setValidator(QIntValidator())
+
             button_hover = "QPushButton:hover{background:#0B6FFE; color:#FFFFFF;}"
 
             self.checkingButton = QPushButton('Checking')
@@ -56,22 +59,25 @@ class RenderMissingFrames(QWidget):
             grid.setSpacing(10)
             grid.setMargin(20)
 
-            grid.addWidget(self.nodeLabel, 0, 0, Qt.AlignTop)
+            grid.addWidget(self.nodeLabel, 0, 0)
 
             grid.addWidget(self.rangeLabel, 1, 1)
-            hbox = QHBoxLayout()
-            hbox.addWidget(self.startText)
-            hbox.addWidget(self.toLabel)
-            hbox.addWidget(self.endText)
-            hbox.addWidget(self.checkingButton)
-            grid.addLayout(hbox, 1, 2)
-
-            grid.addWidget(self.mfLabel, 2, 1)
-            grid.addWidget(self.mfText, 2, 2)
-
-            grid.addWidget(self.renderButton, 3, 0, 1, -1, Qt.AlignBottom)
-            grid.setRowMinimumHeight(3, 60)
+            grid.addWidget(self.startText, 1, 2)
+            grid.addWidget(self.toLabel, 1, 3)
+            grid.addWidget(self.endText, 1, 4)
+            grid.addWidget(self.checkingButton, 1, 5)
             
+            grid.addWidget(self.mfLabel, 2, 1)
+            grid.addWidget(self.mfText, 2, 2, 1, 4)
+
+            grid.addWidget(self.renderButton, 3, 1, 1, 5, Qt.AlignBottom)
+            grid.setRowMinimumHeight(3, 40)
+
+            grid.addWidget(QLabel(''), 0, 6)
+
+            grid.setColumnMinimumWidth(0, 20)
+            grid.setColumnMinimumWidth(6, 20)
+
             self.setLayout(grid)
 
     def setPZ(self):
@@ -87,27 +93,29 @@ class RenderMissingFrames(QWidget):
 
     def missingFrames(self):
         frameNum = self.node['file'].getValue().split('.')[-2]
-        evalStr = self.node['file'].evaluate().rsplit('.', 2)
-        seq = '%s.%s.%s'%(evalStr[0], frameNum, evalStr[-1])
+        
+        if frameNum.startswith('%') or frameNum.startswith('#'):
+            evalStr = self.node['file'].evaluate().rsplit('.', 2)
+            seq = '%s.%s.%s'%(evalStr[0], frameNum, evalStr[-1])
 
-        self.frames = []
-        for frame in range(self.start, self.end+1):
-            file = seq%frame
-            if not os.path.exists(file):
-                self.frames.append(frame)
+            self.frames = []
+            for frame in range(self.start, self.end+1):
+                file = seq%frame
+                if not os.path.exists(file):
+                    self.frames.append(frame)
 
-        self.missingNums = ''
-        if self.frames:
-            for frame in self.frames:
-                if frame-1 in self.frames:
-                    if frame-2 in self.frames:
-                        self.missingNums = self.missingNums.replace(str(frame-1), str(frame))
+            self.missingNums = ''
+            if self.frames:
+                for frame in self.frames:
+                    if frame-1 in self.frames:
+                        if frame-2 in self.frames:
+                            self.missingNums = self.missingNums.replace(str(frame-1), str(frame))
+                        else:
+                            self.missingNums += '-%s'%frame
                     else:
-                        self.missingNums += '-%s'%frame
-                else:
-                    self.missingNums += ' %s'%frame
+                        self.missingNums += ' %s'%frame
 
-        self.missingNums = self.missingNums.strip()
+            self.missingNums = self.missingNums.strip()
 
     def checking(self):
         try:
